59f44ad51969d26adbe921a2e9a4f6d59c124768
kaushikr64 <116604237+kaushikr64@users.noreply.github.com>
2024-12-27 00:21:38 -0500

    done

--- Code/Subroutines/Utility/fig2pdf.m
+++
@@ -1,8 +0,0 @@
-function [] = fig2pdf(file,GraphicsPath)
-%SAVEFIG2PDF Take a guess
-    savedFig = [file,'.fig'];
-    openfig(savedFig);
-    exportgraphics(gcf, fullfile(GraphicsPath,[file,'.pdf']),Resolution=150);
-    close all
-end
-
